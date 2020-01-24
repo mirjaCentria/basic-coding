fdfc162fe479c59a6ce382146f59f1842af13a54
mirjaCentria <mirja.uusitalo@centria.fi>
2020-01-24 13:41:54 +0200

    done and tested

--- part1/repetition/exercise_39/Program.cs
+++ part1/repetition/exercise_39/Program.cs
@@ -6,7 +6,22 @@ namespace exercise_39
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      int sum = 0; 
+      int number = 0; 
+
+      string sPrompt ="Give a number:";
+      string sDone ="Total sum of numbers: ";
+
+      while (true )     
+      {
+        Console.WriteLine(sPrompt);
+        number = Convert.ToInt32(Console.ReadLine());  
+
+        if(number == 0) break;
+        sum = sum + number;
+
+      }  
+      Console.WriteLine(sDone + sum);
     }
   }
 }
