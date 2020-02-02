cd9088d9f883d45dbe74bf5f9aff275631e63034
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 15:21:40 +0200

    done and tested

--- part2/moreLoops/exercise_46/Program.cs
+++ part2/moreLoops/exercise_46/Program.cs
@@ -6,8 +6,17 @@ namespace exercise_46
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+      /** Write a program, which reads an integer from the user. 
+      Then the program prints numbers from that number to 100.
+       You can assume that the user always gives a number 
+       less than 100.*/ 
+       
+        int max = 100;
+        int nmbr = Convert.ToInt32(Console.ReadLine());
+        while (nmbr < max + 1)
+        {            
+            Console.WriteLine(nmbr++);
+        } 
 
     }
   }
