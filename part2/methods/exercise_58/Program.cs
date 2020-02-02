5b2f8f1daf60df1200d925baaa118cdc706864fd
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 20:46:31 +0200

    done and tested

---
+++ part2/methods/exercise_58/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace exercise_58
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+      int answer = Smallest(2, 7);
+      Console.WriteLine("Smallest: " + answer);
+    }
+
+  
+     public static int Smallest(int number1, int number2)
+    {
+      if(number1 > number2) return number2;
+      else return number1;
+    }
+    
+  }
+}
