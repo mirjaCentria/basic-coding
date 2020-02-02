4402496636817d0a445132de3bca596414393b66
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 19:08:55 +0200

    done and tested

---
+++ part2/methods/exercise_51/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise_51
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+      PrintUntilNumber(5);
+
+    }
+
+    public static void PrintUntilNumber(int number)
+    {
+      for(int i = 1; i < number + 1; i++)
+      { 
+        Console.WriteLine(i);
+      }
+    }
+
+  }
+}
