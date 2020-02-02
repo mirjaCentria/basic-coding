374434377c276dbdd574135f703b2f3c8bfb3605
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 14:58:18 +0200

    done and tested

--- part2/subproblems/exercise_43/Program.cs
+++ part2/subproblems/exercise_43/Program.cs
@@ -6,8 +6,11 @@ namespace exercise_43
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-      
+      /*Write a program, that reads an integer from the user. If the number is less than 0, it prints the number multiplied by -1. Otherwise the program prints the number itself.
+       A few examples of the expected function below:*/
+        int nmbr = Convert.ToInt32(Console.ReadLine());
+        if(nmbr < 0) nmbr = nmbr*(-1);
+        Console.WriteLine(nmbr);  
     }
   }
 }
--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -6,8 +6,19 @@ namespace exercise_44
   {
     public static void Main(string[] args)
     {
+      /*Write a program that reads two integers from the user input.
+      If the first number is greater than the second, the program prints 
+      "(first) is greater than (second)." If the first number is less than the second, the program prints "(first) is less than (second)." Otherwise the program prints "(first) is equal to (second)." The (first) and (second) should always be replaced 
+      with the actual numbers given by the user.*/
 
-      // Write your code here:
+        string lrg = " is greater than ";
+        string sml = " is less than ";
+        string eq = " is equal to ";
+        int nmbr1 = Convert.ToInt32(Console.ReadLine());
+        int nmbr2 = Convert.ToInt32(Console.ReadLine());
+        if(nmbr1 > nmbr2) Console.WriteLine(nmbr1+ lrg + nmbr2 + ".");
+        else if(nmbr1 < nmbr2) Console.WriteLine(nmbr1 + sml + nmbr2 + ".");
+        else if(nmbr1 == nmbr2) Console.WriteLine(nmbr1 + eq + nmbr2 + ".");
 
     }
   }
