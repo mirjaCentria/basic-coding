c50529b3fcf5c4a8d6783217946efb3fd781c030
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 21:34:53 +0200

    done and tested

---
+++ part2/methods/exercise_59/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercise_59
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+      int answer = Greatest(2, 7, 3);
+      Console.WriteLine("Greatest: " + answer);
+    }
+
+    public static int Greatest(int number1, int number2, int number3) 
+    {
+      if(number1 >= number2) 
+      {
+        if(number1 >= number3) return number1;
+        else return number3;
+      } 
+      else if(number2 >= number3) return number2;
+      else return number3;
+      
+    }
+
+  }
+}
