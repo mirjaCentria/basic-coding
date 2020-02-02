0487d232f4bb652d4be666591f85dc91431bb48b
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 20:13:10 +0200

    doneand tested

---
+++ part2/methods/exercise_55/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise_55
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+        int result = NumberUno();
+        
+    }
+
+      public static int NumberUno()
+      {
+        return 1;
+      }
+  }
+}
