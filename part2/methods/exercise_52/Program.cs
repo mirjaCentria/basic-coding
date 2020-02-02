d40ee9ab56236346a662cfb868e9ffe684a7d5b7
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 19:10:25 +0200

    done and tested

---
+++ part2/methods/exercise_52/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercise_52
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+      PrintFromNumberToOne(5);
+
+    }
+
+    /** Create the following method in the exercise template:
+     **public static void PrintFromNumberToOne(int number)**. 
+     It should print the numbers from the number passed as a parameter
+      down to one. Two examples of the method's usage are given below.   
+    */
+    public static void PrintFromNumberToOne(int number)
+    {
+      while(number > 0)
+      { 
+        Console.WriteLine(number);
+        number--;
+      }
+    }
+
+  }
+}
