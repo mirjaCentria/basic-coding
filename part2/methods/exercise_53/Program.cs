ef866fd31a937091c18f125319614bf96a699d41
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 19:24:50 +0200

    comma2dot

---
+++ part2/methods/exercise_53/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise_53
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+      Division(2, 3);
+    }
+
+    public static void Division(int numerator, int denominator)
+    {
+      double result = (double)numerator/denominator;
+      Console.WriteLine(result);
+    }
+  }
+}
