12abd115458a2ee48f5d610c12e3f40751526274
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 22:02:16 +0200

    done and tested

---
+++ part2/methods/exercise_60/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace exercise_60
+{
+  class Program
+  {
+        public static void Main(String[] args)
+    {
+        PrintStars(5);
+        PrintStars(1);
+        PrintStars(0);
+        PrintSquare(4);
+        PrintRectangle(17, 4);
+        PrintTriangle(5);
+
+    }
+
+
+    public static void PrintStars(int number)
+    {
+
+      for(int i = 0; i < number; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
+    }
+
+    
+    public static void PrintSpace(int number)
+    {
+
+      for(int i = 0; i < number; i++)
+      {
+        Console.Write(" ");
+      }      
+    }
+
+    public static void PrintSquare(int size)
+    {
+      for(int i = 0; i < size; i++)
+      {
+        PrintStars(size);
+      }  
+    }
+
+    public static void PrintRectangle(int width, int height)
+    {
+      for(int i = 0; i < height; i++)
+      {
+        PrintStars(width);
+      } 
+    }
+
+    public static void PrintTriangle(int size)
+    {
+      for(int i = 0; i < size; i++)
+      {
+        PrintStars(i + 1);
+      }
+    }
+  }
+}
