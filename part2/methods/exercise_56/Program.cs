8902171fa7f251fba1e1e0b7c7c8ffc3ed7e263d
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 20:17:05 +0200

    done and tested

---
+++ part2/methods/exercise_56/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercise_56
+{
+  class Program
+  {
+    public static void Main(String[] args)
+    {
+        string result = Word();
+        
+    }
+
+      public static string Word()
+      {
+        return "blaa";
+      }
+  }
+}
