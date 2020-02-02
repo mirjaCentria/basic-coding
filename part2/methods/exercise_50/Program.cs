475c4b70cd09e861a1dc8bb5fdfd5ea39b9afc88
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-02-02 18:46:34 +0200

    done and tested

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -4,16 +4,19 @@ namespace exercise_50
 {
   class Program
   {
-    public static int Sum(int number1, int number2, int number3, int number4)
+    public static void Main(string[] args)
     {
-      // write your code here
-      // remember to include return (at the end)!
+      Console.WriteLine("How many times?");
+      int nmbr = Convert.ToInt32(Console.ReadLine());
+      for(int i = 0; i < nmbr; i++)
+      {
+        PrintPhrase();
+      }  
     }
 
-    public static void Main(String[] args)
-    {
-      int answer = Sum(4, 3, 6, 1);
-      Console.WriteLine("Sum: " + answer);
+    public static void PrintPhrase() {
+      string testLine = "In a hole in the ground there lived a method";
+      Console.WriteLine(testLine);
     }
   }
 }
