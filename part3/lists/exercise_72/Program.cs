889ac5d2d75d5e3170652bae1b539e7e34a9f581
mirjaCentria <mirja.uusitalo@centria.fi>
2020-02-05 15:10:12 +0200

    done and tested

--- part3/lists/exercise_72/Program.cs
+++ part3/lists/exercise_72/Program.cs
@@ -7,7 +7,37 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
+      List<int> list = new List<int>();
+      while (true)
+      {
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input == 9999)
+        {
+          break;
+        }
+        list.Add(input);
+      }
 
+      bool flag = false;
+      int smallest = 9999;
+      
+      foreach(int item in list)
+      {
+        if(item < smallest) smallest = item;
+      }
+
+      for(int i = 0; i < list.Count; i++)
+      {
+        if(list[i] == smallest) 
+        {
+          if(!flag)
+          {          
+            Console.WriteLine("Smallest number: " + smallest);
+            flag = true;
+          }
+          Console.WriteLine("Found at index: " + i);
+        }      
+      }        
     }
   }
 }
