07f25ebe7e77d7a2aaebdc6cef56a7abe383a77e
mirjaCentria <mirja.uusitalo@centria.fi>
2020-02-05 11:41:56 +0200

    done and tested

--- part3/lists/exercise_62/Program.cs
+++ part3/lists/exercise_62/Program.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 
 
@@ -22,40 +21,9 @@ namespace exercise_62
       }
 
       // BEGIN SOLUTION
-      Console.WriteLine(list[0]);
+      Console.WriteLine(list[2]);
       // END SOLUTION
     }
 
   }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-
-
-namespace exercise_62
-{
-  class Program
-  {
-    public static void Main(string[] args)
-    {
-      List<string> list = new List<string>();
-      while (true)
-      {
-        string input = Console.ReadLine();
-        if (input == "")
-        {
-          break;
-        }
-
-        list.Add(input);
-      }
-
-      // BEGIN SOLUTION
-      Console.WriteLine(list[0]);
-      // END SOLUTION
-    }
-
-  }
-}
->>>>>>> b0e4e5eb6530598e59326b1a0dffa71e0e93d10b
