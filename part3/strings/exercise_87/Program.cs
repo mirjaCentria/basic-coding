77b544ad30aa0eecfc7ebd70f4005a57b55ddede
mirjaCentria <mirja.uusitalo@centria.fi>
2020-02-06 13:58:44 +0200

    done and tested

--- part3/strings/exercise_87/Program.cs
+++ part3/strings/exercise_87/Program.cs
@@ -7,10 +7,21 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
+      string line;
+    
+      while(true)
+      {
+        line = Console.ReadLine();
+        if(line == "") break;
+        string[] words = line.Split(" ");
 
+        foreach(string word in words)
+        {
+          if(word.Contains("av"))
+            Console.WriteLine(word);
+        }
+      }
     }
-
   }
-
 }
 
