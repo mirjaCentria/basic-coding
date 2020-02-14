c5db1ea87403ab60a00ed0476e311e5613607f36
mirjaCentria <mirja.uusitalo@centria.fi>
2020-02-14 11:08:07 +0200

    done and tested

---
+++ part4/objects/exercise_109/PaymentCard.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace exercise_109 
+{  public class PaymentCard
+  {
+    private double balance;
+
+    public PaymentCard(double openingBalance)
+    {
+      this.balance = openingBalance;
+    }
+
+    public override string ToString()
+    {
+        string line = "The card has a balance of " + this.balance + " euros";
+        return line;
+    }
+
+    public void EatLunch()
+    {
+      double lunch = 10.60;
+      if(this.balance > lunch) this.balance = this.balance - lunch;
+    }
+
+    public void DrinkCoffee()
+    {
+      double coffee = 2.0;
+      if(this.balance > coffee) this.balance = this.balance - coffee;
+    }
+
+    public void AddMoney(double amount)
+    {
+      double limit = 150;
+      if(amount > 0) this.balance = this.balance + amount;
+      if(this.balance > limit) this.balance = limit;
+    }
+  }
+}
