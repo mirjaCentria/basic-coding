02b145e6eee28b6ce17c23ceeeb17f19c1666438
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-03-11 22:30:28 +0200

    done

--- extra/extra_10/Program.cs
+++ extra/extra_10/Program.cs
@@ -2,12 +2,25 @@
 
 namespace extra_10
 {
-  class Program
+  class Program     
   {
     public static void Main(string[] args)
     {
-      // Add your code here:
+      int[] array = {5, 1, 3, 4, 2};
+      PrintArrayInStars(array);
 
+    }
+
+    public static void PrintArrayInStars(int[] array) 
+    {
+      for(int i = 0; i < array.Length; i++)
+      {
+        for(int j = 0; j <  array[i]; j++)
+        {
+          Console.Write("*");
+        }
+        Console.WriteLine("");
+      }
     }
   }
 }
