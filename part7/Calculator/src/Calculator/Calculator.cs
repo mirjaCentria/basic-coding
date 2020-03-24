1d9072a72408b6be5e0a5c2bd69d6f7aeb60767b
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-03-24 21:09:00 +0200

    Done and tested

---
+++ part7/Calculator/src/Calculator/Calculator.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Calculators
+{
+  public class Calculator
+  {
+    public int value { get; private set; }
+
+    public Calculator()
+    {
+      this.value = 0;
+    }
+
+    public void Sum(int number)
+    {
+      this.value = this.value + number;
+    }
+
+    public void Substract(int number)
+    {
+      this.value = this.value - number;
+    }
+  }
+}
