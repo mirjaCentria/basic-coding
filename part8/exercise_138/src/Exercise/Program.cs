48f8d2c71bb309a5a0f5b41812617236d3d97a15
mirjaCentria <60070753+mirjaCentria@users.noreply.github.com>
2020-03-30 17:30:10 +0300

    done and tested

--- part8/exercise_138/src/Exercise/Program.cs
+++ part8/exercise_138/src/Exercise/Program.cs
@@ -1,4 +1,4 @@
-using System;
+   using System;
 using System.Collections.Generic;
 
 namespace Exercise
@@ -7,9 +7,17 @@ namespace Exercise
   {
     public static void Main(string[] args)
     {
-      // Do something here
+      Dictionary<string, string> names = new Dictionary<string, string>();
+      names.Add("matthew", "matt");
+      names.Add("michael", "mix");
+      names.Add("arthur", "artie");
 
+      foreach( KeyValuePair<string, string> kvp in names )
+      {
+          System.Console.WriteLine("{0}'s nickname is {1}", 
+              kvp.Key, kvp.Value);
       }
+     
     }
   }
 }
--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -4,20 +4,35 @@ namespace Exercise
 {
   public class Abbreviations
   {
+     Dictionary<string, string> abbs;
+      
+     
+    public Abbreviations()
+    {
+      this.abbs = new Dictionary<string, string> ();
+    }
 
     public void AddAbbreviation(string abbreviation, string explanation)
     {
-
+      this.abbs.Add(abbreviation, explanation);
     }
 
     public bool HasAbbreviation(string abbreviation)
     {
+      if (this.abbs.ContainsKey(abbreviation))
+      {
+        return true;
+      }
       return false;
     }
 
     public string FindExplanationFor(string abbreviation)
     {
-      return null;
+      if(this.HasAbbreviation(abbreviation))
+      {
+        return this.abbs[abbreviation];
+      }
+      return "not found";
     }
   }
 }
