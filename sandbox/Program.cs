dfe3e7aac1076d9c09a072b0134153eabc32063d
mirjaCentria <connectedteamCentria@gmail.com>
2020-04-25 00:30:19 +0300

    done and tested

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -7,21 +7,10 @@
   {
     static void Main(string[] args)
     {
-      //Regex regex = new Regex("^01[0-9]{7}$");
-     // Regex regex = new Regex("^mon|tue|wed|thu|fri|sat|sun$");
-        Regex rgx = new Regex("^[aeiou]$");
+   
 
-      Console.Write("Provide a student number: ");
-      string number = Console.ReadLine();
+      Console.Write("Hello world");
 
-      if (rgx.IsMatch("akkkkkkka"))
-      {
-        Console.WriteLine("consonants");
-      }
-      else
-      {        
-        Console.WriteLine("wowels only");
-      }
     }
   }
 }
