f895e33e637990628e722098dade5f1085adad95
mirjaCentria <mirja.uusitalo@centria.fi>
2020-05-08 00:39:40 +0300

    done and tested

---
+++ part12/exercise_166/src/Exercise/Namespaces/NamespaceExample.cs
@@ -0,0 +1,22 @@
+namespace NamespaceExample 
+{
+  using System;
+  class Example
+  {
+    public void ExampleMethod()
+    {
+      Console.WriteLine("ExampleMethod in NamespaceExample");
+    }
+  }
+
+  namespace InnerNamespaceExample {
+    // This should be nested
+    class Example
+    {
+      public void ExampleMethod()
+      {
+        Console.WriteLine("ExampleMethod in InnerNamespaceExample");
+      }
+    }
+  }
+}
